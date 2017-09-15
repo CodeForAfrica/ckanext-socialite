96fbdfbdd537ff780eb8330b27e91bf21c4cbad8
Adrian Otieno <adrianadero.otieno@gmail.com>
2017-09-15 22:44:39 +0300

    [Chore] Correct extension tags

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
 
 
     # What does your project relate to?
-    keywords='''CKAN "ckan ckan-extension login oauth2"''',
+    keywords='''CKAN ckan ckan-extension login oauth2''',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().

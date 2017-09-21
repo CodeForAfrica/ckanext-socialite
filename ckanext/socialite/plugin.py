1fafb3664f33cb330173a6a43262d094745a50c4
Mercy-Muchai <mercy.muchai@andela.com>
2017-09-21 11:20:55 +0300

    grab username from email

--- ckanext/socialite/plugin.py
+++ ckanext/socialite/plugin.py
@@ -33,12 +33,12 @@ import re
 
 
 
-#get 'ckan.googleauth_clientid' from ini file
+# get 'ckan.googleauth_clientid' from ini file
 def get_clientid():
     return config.get('ckan.googleauth_clientid', '')
 
 
-#get ckan.googleauth_hosted_domain from ini file
+# get ckan.googleauth_hosted_domain from ini file
 def get_hosted_domain():
     return config.get('ckan.googleauth_hosted_domain', '')
 
@@ -47,131 +47,117 @@ class GoogleAuthException(Exception):
     pass
 
 
-
 class SocialitePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IAuthenticator)
     plugins.implements(plugins.ITemplateHelpers)
-
-
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'googleauth')
 
-
-    #declare new helper functions
+    # declare new helper functions
     def get_helpers(self):
         return {'googleauth_get_clientid': get_clientid,
-		'googleauth_get_hosted_domain': get_hosted_domain}
+                'googleauth_get_hosted_domain': get_hosted_domain}
 
-
-
-    #verify email address within token
+    # verify email address within token
     def verify_email(self, token):
-        res = requests.post('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token='+token, verify=True)
+        res = requests.post(
+            'https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=' + token, verify=True)
 
         if res.ok:
-                is_email_verified=json.loads(res.content)
-                if is_email_verified['email_verified'] == 'true':
-                        email_verified = is_email_verified['email']
-                        return email_verified
-                else:
-                        raise GoogleAuthException(is_email_verified)
+            is_email_verified = json.loads(res.content)
+            if is_email_verified['email_verified'] == 'true':
+                email_verified = is_email_verified['email']
+                return email_verified
+            else:
+                raise GoogleAuthException(is_email_verified)
         else:
-                raise GoogleAuthException(res)
+            raise GoogleAuthException(res)
 
-
-
-    #if exist returns ckan user
+    # if exist returns ckan user
     def get_ckanuser(self, user):
-    	import ckan.model
+        import ckan.model
 
-	user_ckan = ckan.model.User.by_name(user)
+        user_ckan = ckan.model.User.by_name(user)
 
-	if user_ckan:
-		user_dict = toolkit.get_action('user_show')(data_dict={'id': user_ckan.id})
-		return user_dict
-	else:
-		return None
+        if user_ckan:
+            user_dict = toolkit.get_action('user_show')(
+                data_dict={'id': user_ckan.id})
+            return user_dict
+        else:
+            return None
 
-
-
-    #generates a strong password
+    # generates a strong password
     def get_ckanpasswd(self):
-	import datetime
-	import random
+        import datetime
+        import random
 
-	passwd = str(random.random())+ datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")+str(uuid.uuid4().hex)
-	passwd = re.sub(r"\s+", "", passwd, flags=re.UNICODE)
-	return passwd
-
-
+        passwd = str(random.random()) + datetime.datetime.now().strftime(
+            "%Y-%m-%d %H:%M:%S.%f") + str(uuid.uuid4().hex)
+        passwd = re.sub(r"\s+", "", passwd, flags=re.UNICODE)
+        return passwd
 
     def _logout_user(self):
         #import pylons
 
-	#to revoke the Google token uncomment the code below
+        # to revoke the Google token uncomment the code below
 
-	#if 'ckanext-google-accesstoken' in pylons.session:
-	#    atoken = pylons.session.get('ckanext-google-accesstoken')
-	#    res = requests.get('https://accounts.google.com/o/oauth2/revoke?token='+atoken)
-	#    if res.status_code == 200:
-	#    	del pylons.session['ckanext-google-accesstoken']
-	#    else:
-	#	raise GoogleAuthException('Token not revoked')
+        # if 'ckanext-google-accesstoken' in pylons.session:
+        #    atoken = pylons.session.get('ckanext-google-accesstoken')
+        #    res = requests.get('https://accounts.google.com/o/oauth2/revoke?token='+atoken)
+        #    if res.status_code == 200:
+        #    	del pylons.session['ckanext-google-accesstoken']
+        #    else:
+        #	raise GoogleAuthException('Token not revoked')
         if 'ckanext-google-user' in pylons.session:
             del pylons.session['ckanext-google-user']
         if 'ckanext-google-email' in pylons.session:
             del pylons.session['ckanext-google-email']
         pylons.session.save()
 
-
-
-    #at every access the email address is checked. if it is authorized ckan username is created and access is given
+    # at every access the email address is checked. if it is authorized ckan username is created and access is given
     def login(self):
 
-    	params = toolkit.request.params
+        params = toolkit.request.params
 
-	if 'id_token' in params:
-		try:
-			mail_verified = self.verify_email(params['id_token'])
-		except GoogleAuthException, e:
-			toolkit.abort(500)
+        if 'id_token' in params:
+            try:
+                mail_verified = self.verify_email(params['id_token'])
+            except GoogleAuthException, e:
+                toolkit.abort(500)
 
-		user_account = re.sub('[^A-Za-z0-9]+','_',mail_verified)
+            # user_account = re.sub('[^A-Za-z0-9]+', '_', mail_verified)
+            user_account = mail_verified.split('@')[0]
+            if user_account.isalnum() is False:
+                user_account = ''.join(e for e in user_account if e.isalnum())
+        
+            user_ckan = self.get_ckanuser(user_account)
 
-		user_ckan = self.get_ckanuser(user_account)
+            if not user_ckan:
+                user_ckan = toolkit.get_action('user_create')(
+                    context={'ignore_auth': True},
+                    data_dict={'email': mail_verified,
+                               'name': user_account,
+                               'password': self.get_ckanpasswd()})
 
-		if not user_ckan:
-			user_ckan = toolkit.get_action('user_create')(
-                    				context={'ignore_auth': True},
-                    				data_dict={'email': mail_verified,
-                               			'name': user_account,
-                               			'password': self.get_ckanpasswd()})
+            pylons.session['ckanext-google-user'] = user_ckan['name']
+            pylons.session['ckanext-google-email'] = mail_verified
 
-		pylons.session['ckanext-google-user'] = user_ckan['name']
-        	pylons.session['ckanext-google-email'] = mail_verified
+            # to revoke the Google token uncomment the code below
+            #pylons.session['ckanext-google-accesstoken'] = params['token']
+            pylons.session.save()
 
-		#to revoke the Google token uncomment the code below
-		#pylons.session['ckanext-google-accesstoken'] = params['token']
-            	pylons.session.save()
-
-
-
-    #if someone is logged in will be set the parameter c.user
+    # if someone is logged in will be set the parameter c.user
     def identify(self):
-	user_ckan = pylons.session.get('ckanext-google-user')
+        user_ckan = pylons.session.get('ckanext-google-user')
         if user_ckan:
             toolkit.c.user = user_ckan
 
-
-
     def logout(self):
-	self._logout_user()
-
-
+        self._logout_user()
 
     def abort(self):
-	self._logout_user()
+        self._logout_user()

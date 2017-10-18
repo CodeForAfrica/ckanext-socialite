44699e4c5b6392044e199f2ecc672274968972fd
Adrian Otieno <adrianadero.otieno@gmail.com>
2017-10-18 17:16:26 +0300

    [Chore] Remove non-existent dependency

--- ckanext/socialite/tests/test_e2e/test_socialite.py
+++ ckanext/socialite/tests/test_e2e/test_socialite.py
@@ -1,12 +1,12 @@
 import unittest
-from unnecessary_math import multiply
- 
-class TestUM(unittest.TestCase): 
+
+class TestUM(unittest.TestCase):
     def setUp(self):
         pass
- 
+
     def test_numbers_3_4(self):
-        self.assertEqual( multiply(3,4), 12)
- 
+        self.assertEqual((3 * 4), 12)
+
+
 if __name__ == '__main__':
     unittest.main()

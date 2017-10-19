3a35d81754ffbfb554431d8270d6546822908735
Adrian Otieno <adrianadero.otieno@gmail.com>
2017-10-19 11:29:54 +0300

    [Chore] Ensure file follows testing conventions

--- ckanext/socialite/tests/test_plugin.py
+++ ckanext/socialite/tests/test_plugin.py
@@ -3,7 +3,7 @@ import unittest
 from ckanext.socialite import plugin
 from mock import MagicMock
 
-class SocialitePluginTest(unittest.TestCase):
+class TestSocialitePlugin(unittest.TestCase):
     def setUp(self):
         # Create instances
         self.socialite_instance = plugin.SocialitePlugin()
@@ -12,20 +12,26 @@ class SocialitePluginTest(unittest.TestCase):
 
     def tearDown(self):
         plugin.toolkit = self._toolkit
-        
+
     def test_update_config(self):
-        """Tests that CKAN uses the plugin's custom templates"""
+        """Test that CKAN uses the plugin's custom templates."""
         config = MagicMock()
         self.socialite_instance.update_config(config)
         plugin.toolkit.add_template_directory.assert_called_once_with(config, 'templates')
 
     def test_login(self):
-        """Tests for successful login"""
+        """Test for successful login."""
         params = ([('name', 'Shani Agent'), ('email', 'buzzdhani@hotmail.com'), ('id_token', 'sPDwypON4z')])
         user_account = params[1][1].split('@')[0]
 
         user_ckan = self.socialite_instance.get_ckanuser(user_account)
         self.assertEqual(user_ckan['display_name'], 'Shani Agent')
+
+    def test_passwdcreation(self):
+        """Test the CKAN password is created."""
+        ckan_passwd = self.socialite_instance.get_ckanpasswd()
+        self.assertIsNotNone(ckan_passwd)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
